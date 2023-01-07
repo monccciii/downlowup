ee6a60042c5dd38c0a42a9d41fc936651e6f35f9
monccciii <44330082+monccciii@users.noreply.github.com>
2023-01-07 13:03:40 -0500

    Update landing.tsx

--- components/landing.tsx
+++ components/landing.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { motion } from 'framer-motion'
 import saturatedbg from '../public/saturatedbg.png'
 import { useEffect, useState } from 'react'
-import Bring2top from './components/bring2top'
+import Bring2top from './bring2top'
 import emailjs from '@emailjs/browser';
 import { useRef } from 'react';
 import { useRouter } from 'next/router'
